643a9edfa885c19b75e6ffe97f0a0f03c544d344
sa11e <erik.sahlmen@gmail.com>
2018-12-12 09:52:01 +0100

    Continued on the app

--- RiddleOfToday/RiddleOfToday/MainPage.xaml.cs
+++ RiddleOfToday/RiddleOfToday/MainPage.xaml.cs
@@ -9,9 +9,54 @@ namespace RiddleOfToday
 {
 	public partial class MainPage : ContentPage
 	{
+	    private RiddleOfTodayService.Riddle riddle;
 		public MainPage()
 		{
 			InitializeComponent();
 		}
-	}
+
+	    //public void OnColorSliderChanged(Object sender, EventArgs e)
+	    //{
+	    //    var red = sliderRed.Value;
+	    //    var green = sliderGreen.Value;
+	    //    var blue = sliderBlue.Value;
+
+	    //    boxViewColor.Color = Color.FromRgb(red, green, blue);
+	    //}
+
+	    public async void GetRiddleButton_Clicked(Object sender, EventArgs e)
+	    {
+	        try
+	        {
+	            InfoLabel.Text = "Hämtar dagens gåta...";
+                RiddleQuestionLabel.Text = "";
+	            RiddleAnswerLabel.Text = "";
+                riddle = await RiddleOfTodayService.GetRiddleOfToday();
+
+	            InfoLabel.Text = "";
+                RiddleQuestionLabel.Text = riddle.Question;
+	        }
+	        catch (Exception exception)
+	        {
+	            Console.WriteLine(exception);
+	            throw;
+	        }
+	    }
+
+	    public void ShowAnswerButton_Clicked(object sender, EventArgs e)
+	    {
+	        if (riddle == null)
+	            return;
+
+	        if (riddle.Answer != null)
+	        {
+	            RiddleAnswerLabel.Text = riddle.Answer;
+            }
+	        BottomInfo.Text = $"{riddle.LastDisplayDateTime:yyyy-MM-dd} Antal visningar: {riddle.Views}";
+
+            //RiddleDate.Text = riddle.LastDisplayDate;
+            //RiddleDate.Text = riddle.LastDisplayDateTime.ToString("yyyy-MM-dd");
+            //RiddleNoOfDisplays.Text = riddle.Views.ToString();
+        }
+    }
 }
---
+++ RiddleOfToday/RiddleOfToday/RiddleService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiddleOfToday
+{
+    public class RiddleOfTodayService
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task<Riddle> GetRiddleOfToday()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var uri = "https://kingsbackmath.azurewebsites.net/Riddle/GetRiddleOfToday";
+
+            var result = await client.GetStringAsync(uri);
+
+            var riddle = Newtonsoft.Json.JsonConvert.DeserializeObject<Riddle>(result);
+
+            return riddle;
+        }
+
+        [DataContract(Name = "riddle")]
+        public class Riddle
+        {
+            [DataMember(Name = "id")]
+            public int Id { get; set; }
+
+            [DataMember(Name = "question")]
+            public string Question { get; set; }
+
+            [DataMember(Name = "answer")]
+            public string Answer { get; set; }
+
+            [DataMember(Name = "lastDisplayDate")]
+            public string LastDisplayDate { get; set; }
+
+            [IgnoreDataMember]
+            //public DateTime LastDisplayDateTime => DateTime.ParseExact(LastDisplayDate, "yyyy-MM-ddTHH:mm:ss", CultureInfo.CurrentCulture);
+            public DateTime LastDisplayDateTime => DateTime.Parse(LastDisplayDate);
+
+            [DataMember(Name = "views")]
+            public int Views { get; set; }
+
+            [DataMember(Name = "difficulty")]
+            public string Difficulty { get; set; }
+        }
+    }
+}
